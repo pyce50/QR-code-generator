33c0f3bb672c50d6b1a88ea0e63b7b3846857b84
Ростислав <pyce50@gmail.com>
2023-10-21 20:52:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import qrcode
+
+qr = qrcode.QRCode(version=1,
+error_correction=qrcode.constants.ERROR_CORRECT_L,
+box_size=10, border=4)
+qr.add_data('https://github.com/pyce50')
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="red")
+img.save("my_qrcode.png")
